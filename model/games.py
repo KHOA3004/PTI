f526620c72ea68c2bd87c835a66dd27a7d40f7a4
unknown <dodangkhoa3004@gmail.com>
2024-07-28 12:06:53 +0700

    ....

--- model/games.py
+++ model/games.py
@@ -35,6 +35,15 @@ class ListGame:
         return self.list
     def add_games(self, Game):
         self.list.append(Game)
+    def delete_games_by_name(self,name_game):
+        for game in self.list:
+            if game.getname == name_game:
+                self.list.remove(game)
+    def edit_game_by_name(self,name_old_game:str,new_game:Game):
+        for game in self.list:
+            if game.getName() == name_old_game:
+                self.list.remove(game)
+                self.list.append(new_game)
     def show_all_game(self):
         for i in self.list:
             i.show()
@@ -55,7 +64,14 @@ class ListGame:
             jsonfile.append(game.__dict__)
         with open("data/games.json", "w") as file:
             json.dump(jsonfile, file, indent = 5)
+    def searchGameaByName(self,name)->list:
+        result = []
+        for game in self.list:
+            if name in game.getName():
+                result.append(game)
+                game.show()
+        return result
 
-l  = ListGame()
-for i in l.getAllGames():
-    i.open_game()
+L = ListGame()
+L.edit_game_by_name("1",Game("1","Conan","29/10/2004","2","https://www.youtube.com/playlist?list=PLKvoOwlacRoLbAmCd_0HsADRTCD4oo0Mv"))
+L.searchGameaByName("Co")
